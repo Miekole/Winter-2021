4fe3ec881d9cbe28438808a12a45e8dec9248eaf
Miekole <Miekole.ferrazzo@gmail.com>
2021-01-25 18:49:53 -0500

    changed title

--- AI/SDL_Engine-master/src/StartScene.cpp
+++ AI/SDL_Engine-master/src/StartScene.cpp
@@ -45,12 +45,12 @@ void StartScene::handleEvents()
 
 void StartScene::start()
 {
-	const SDL_Color blue = { 0, 0, 255, 255 };
-	m_pStartLabel = new Label("START SCENE", "Consolas", 80, blue, glm::vec2(400.0f, 40.0f));
+	const SDL_Color red = { 255, 0, 0, 255 };
+	m_pStartLabel = new Label("Mudflap Mayhem", "Consolas", 80, red, glm::vec2(400.0f, 40.0f));
 	m_pStartLabel->setParent(this);
 	addChild(m_pStartLabel);
 
-	m_pInstructionsLabel = new Label("Press 1 to Play", "Consolas", 40, blue, glm::vec2(400.0f, 120.0f));
+	m_pInstructionsLabel = new Label("Press 1 to Play", "Consolas", 40, red, glm::vec2(400.0f, 120.0f));
 	m_pInstructionsLabel->setParent(this);
 	addChild(m_pInstructionsLabel);
 
